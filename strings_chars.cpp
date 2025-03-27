e7ecb31c8ab49396fb63af1bd70061ad9366a0b3
edlance1 <edlance@ntiertraining.com>
2025-03-27 13:09:59 -0400

    round 1

---
+++ strings_chars.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  char char1 = 'z';
+  char char2 = 'p';
+
+  string str1 = "Welcome";
+  string str2 = " Students!";
+
+  cout << char1 << endl;
+  cout << char2 << endl;
+  cout << str1 << str2 << endl;
+
+  string str3 = "This forms "  "a single"
+                " line.";
+  string str4 = "But good programmers\
+  probably do it this way the most.";  // TODO: this not workin output not on a single line
+
+  cout << str3 << endl;
+  cout << str4 << endl;
+
+  //std::string str5 = R"(string with \backslash)";
+  std::string str5 = "(string with \backslash)";
+  std::cout << str5 << std::endl; // Output: string with \backslash
+  // TODO: should this one work?
+  //string str6 = R"&%$(string with \backslash)&%$"; 
+
+
+  return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,31 @@
+// https://cplusplus.com/doc/tutorial
+// my first program in C++
+
+/* preprocessing directives must be specified in their own line and do not have to end in ;*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // std::cout << "Hello World!  ";
+    // std::cout << "I'm a C++ program";
+    cout << "Hello World!  ";
+    cout << "I'm a C++ program";
+    cout << endl;
+
+    //variable is a portion of memory to store a value
+    //all 3 examples below are valid way to initialize variable
+   
+    int a = 5;              
+    int b(3);             
+    //The following will not work on this machine:
+    //int c{2}; 
+    
+    a = a + 1; 
+    int result = a - b;
+
+    cout << "result:" << result << endl;
+
+    //terminate the program
+    return 0;
+}
